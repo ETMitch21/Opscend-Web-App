1134d3794f84e4736845327731a9045afde38903
Mitch Evans <mitch@summitphone.repair>
2026-04-18 00:50:54 -0500

    Fix Connect

--- src/app/core/mobilesentrix/mobilesentrix-service.ts
+++ src/app/core/mobilesentrix/mobilesentrix-service.ts
@@ -45,7 +45,7 @@ export class MobileSentrixService {
       mobilesentrixConsumerSecret,
     } = this.appConfig.config;
 
-    const callbackUrl = `${window.location.origin}/api/v1/integrations/mobilesentrix/callback/${encodeURIComponent(this.shopId)}`;
+    const callbackUrl = `${this.appConfig.config.apiBase}/v1/integrations/mobilesentrix/callback/${encodeURIComponent(this.shopId)}`;
 
     const url =
       `${mobilesentrixUrl}/oauth/authorize/identifier` +
